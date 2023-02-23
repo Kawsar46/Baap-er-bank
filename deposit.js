05fef7cf7323557f2d794a171e34b612b45cc5e0
kawsar46 <you@example.com>
2023-02-23 06:27:44 +0600

    add deposit amount

---
+++ deposit.js
@@ -0,0 +1,29 @@
+// step-1 event handler
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    // step-2 get the deposit amount from the input field
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
+    
+//   get the current deposit total amount
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+
+    
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotalElement.innerText = currentBalanceTotal;
+    
+
+
+
+    depositField.value = '';
+})
